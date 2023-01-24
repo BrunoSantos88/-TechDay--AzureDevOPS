033cff476be429bd2ec68d15d00cbebfacb9dac4
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-24 14:41:05 -0300

    tffiles

--- network.tf
+++ network.tf
@@ -1,13 +1,14 @@
 resource "azurerm_virtual_network" "mynetwork" {
   name                = "mynetwork"
   address_space       = ["192.1.0.0/16"]
-  location            = var.resource_group_name_location
-  resource_group_name = var.resource_group_name_group
+  location            = "eastus"
+  resource_group_name = "devops"
 }
+
 
 resource "azurerm_subnet" "mysubnet" {
   name                 = "internal"
-  resource_group_name  = var.resource_group_name_group
+  resource_group_name = "devops"
   virtual_network_name = azurerm_virtual_network.mynetwork.name
   address_prefixes     = ["192.1.0.0/24"]
   service_endpoints    = ["Microsoft.Sql"]
@@ -15,8 +16,8 @@ resource "azurerm_subnet" "mysubnet" {
 
 resource "azurerm_network_interface" "frontend" {
   name                = "frontend-nic"
-  location            = var.resource_group_name_location
-  resource_group_name = var.resource_group_name_group
+  location            = "eastus"
+  resource_group_name = "devops"
 
 
 
@@ -31,31 +32,12 @@ resource "azurerm_network_interface" "frontend" {
 
 resource "azurerm_public_ip" "frontend_ip" {
   name                = "vm_frontend_ip"
-  resource_group_name = var.resource_group_name_group
-  location            = var.resource_group_name_location
+  location            = "eastus"
+  resource_group_name = "devops"
   allocation_method   = "Dynamic"
 }
 
-resource "azurerm_network_interface" "backend" {
-  name                = "backend-nic"
-  location            = var.resource_group_name_location
-  resource_group_name = var.resource_group_name_group
 
-
-  ip_configuration {
-    name                          = "backend"
-    subnet_id                     = azurerm_subnet.mysubnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.backend_ip.id
-  }
-}
-
-resource "azurerm_public_ip" "backend_ip" {
-  name                = "vm_backend_ip"
-  resource_group_name = var.resource_group_name_group
-  location            = var.resource_group_name_location
-  allocation_method   = "Dynamic"
-}
 
 
 
